4c67ba3ea892bca524b3ca4b5fc1c6ce409ebe2a
andreszeballosjuanico@gmail.com <andreszeballosjuanico@gmail.com@02ae3a37-3a1f-261c-a102-2b74b5947da3>
2011-06-27 01:15:49 +0000

    AZ: pronto la baja de cliente

--- trunk/src/comportamiento/CompCliente.java
+++ trunk/src/comportamiento/CompCliente.java
@@ -1,5 +1,27 @@
 package comportamiento;
 
+import java.util.Date;
+import java.util.HashMap;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import taller.Cliente;
+
 public class CompCliente {
 
+	@SuppressWarnings("rawtypes")
+	public void bajaCliente(Cliente cliente, Date baja){
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(
+				"obligatorio", new HashMap());
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		cliente.setBaja(baja);
+
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+	}
 }
---
+++ trunk/src/prueba/PruebaBajaCliente.java
@@ -0,0 +1,53 @@
+package prueba;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import taller.Cliente;
+
+import comportamiento.CompCliente;
+
+public class PruebaBajaCliente {
+
+	@SuppressWarnings({ "rawtypes", "unchecked", "deprecation" })
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(
+				"obligatorio", new HashMap());
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		System.out.println("Clientes antes de la baja:");
+		List<Cliente> l = (List<Cliente>) em.createQuery(
+				"Select m From Cliente m").getResultList();
+		for (int i = 0; i < l.size(); i++) {
+			System.out.println(l.get(i));
+		}
+
+		em.getTransaction().commit();
+
+		CompCliente cp = new CompCliente();
+
+		Date fecha = new Date(Date.UTC(111, 5, 26, 12, 0, 0));
+		System.out.println(fecha);
+
+		Cliente cliente = em.find(Cliente.class, 2);
+
+		cp.bajaCliente(cliente, fecha);
+
+		em.getTransaction().begin();
+
+		System.out.println("Clientes luego de la baja:");
+		l = (List<Cliente>) em.createQuery("Select m From Cliente m")
+				.getResultList();
+		for (int i = 0; i < l.size(); i++) {
+			System.out.println(l.get(i));
+		}
+		em.getTransaction().commit();
+
+	}
+}
