0cf2ca93b64b904047066b603aad46ca935015a2
andreszeballosjuanico@gmail.com <andreszeballosjuanico@gmail.com@02ae3a37-3a1f-261c-a102-2b74b5947da3>
2011-06-27 00:31:25 +0000

    AZ: queda pronto el altaPresupuesto.

--- trunk/src/comportamiento/CompPresupuesto.java
+++ trunk/src/comportamiento/CompPresupuesto.java
@@ -1,5 +1,35 @@
 package comportamiento;
 
+import java.util.Date;
+import java.util.HashMap;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import taller.Auto;
+import taller.Presupuesto;
+
 public class CompPresupuesto {
 
+	@SuppressWarnings("rawtypes")
+	public void altaPresupuesto(Date fecha, int tiempoReparacion,
+			int costoReparacion, Auto auto) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(
+				"obligatorio", new HashMap());
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		Presupuesto p = new Presupuesto();
+		p.setFecha(fecha);
+		p.setTiempoReparacion(tiempoReparacion);
+		p.setCostoReparacion(costoReparacion);
+		p.setAuto(auto);
+
+		em.persist(p);
+
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+	}
 }
