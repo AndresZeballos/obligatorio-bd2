89b94f1a65a3a066ccec4aa148f1bebdc7d06fc3
andreszeballosjuanico@gmail.com <andreszeballosjuanico@gmail.com@02ae3a37-3a1f-261c-a102-2b74b5947da3>
2011-06-27 22:43:58 +0000

    AZ: Quedo pronta la segnda consulta.

--- trunk/src/comportamiento/CompPresupuesto.java
+++ trunk/src/comportamiento/CompPresupuesto.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TemporalType;
 
 import taller.Auto;
 import taller.Presupuesto;
@@ -31,5 +32,29 @@ public class CompPresupuesto {
 		em.getTransaction().commit();
 		em.close();
 		emf.close();
+	}
+
+	@SuppressWarnings("rawtypes")
+	public long reparacionesPeriodo(Date inicio, Date fin) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(
+				"obligatorio", new HashMap());
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		long p = (Long) em
+				.createQuery(
+						"SELECT SUM(x.costoReparacion) FROM Presupuesto x "
+								+ "WHERE x.aceptado = true "
+								+ "AND x.fecha BETWEEN :start AND :end")
+				.setParameter("start", inicio, TemporalType.DATE)
+				.setParameter("end", fin, TemporalType.DATE)
+				.getResultList().get(0);
+
+		em.getTransaction().commit();
+
+		em.close();
+		emf.close();
+
+		return p;
 	}
 }
