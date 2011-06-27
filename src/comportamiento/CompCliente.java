5198ac7ba9ca2fdd451777bb1e0b85391b7ce6a6
andreszeballosjuanico@gmail.com <andreszeballosjuanico@gmail.com@02ae3a37-3a1f-261c-a102-2b74b5947da3>
2011-06-27 04:06:40 +0000

    AZ: Pronta mi parte :D

--- src/comportamiento/CompCliente.java
+++ src/comportamiento/CompCliente.java
@@ -2,17 +2,20 @@ package comportamiento;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TemporalType;
 
 import taller.Cliente;
+import taller.Presupuesto;
 
 public class CompCliente {
 
 	@SuppressWarnings("rawtypes")
-	public void bajaCliente(Cliente cliente, Date baja){
+	public void bajaCliente(Cliente cliente, Date baja) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory(
 				"obligatorio", new HashMap());
 		EntityManager em = emf.createEntityManager();
@@ -23,5 +26,32 @@ public class CompCliente {
 		em.getTransaction().commit();
 		em.close();
 		emf.close();
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public List<Presupuesto> reparacionesCliente(Cliente cliente, Date inicio,
+			Date fin) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory(
+				"obligatorio", new HashMap());
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+
+		List<Presupuesto> p = (List<Presupuesto>) em
+				.createQuery(
+						"SELECT x FROM Presupuesto x, Auto a "
+								+ "WHERE a.cliente.id = :id "
+								+ "AND x.auto.id = a.id "
+								+ "AND x.fecha BETWEEN :start AND :end ORDER BY x.fecha")
+				.setParameter("start", inicio, TemporalType.DATE)
+				.setParameter("end", fin, TemporalType.DATE)
+				.setParameter("id", cliente.getId()).getResultList();
+
+		em.getTransaction().commit();
+
+		em.close();
+		emf.close();
+
+		return p;
 	}
 }
